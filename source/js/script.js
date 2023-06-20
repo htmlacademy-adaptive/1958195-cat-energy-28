d3bf7c47e87f9eaeb570be4d173403223132cccb
Alimov Farruh <linsheng1990@gmail.com>
2023-06-20 21:04:02 +0300

    Завершает главную страницу

--- source/js/script.js
+++ source/js/script.js
@@ -1,9 +1,11 @@
-let headerMenu= document.querySelector('.menu');
+let headerMenu = document.querySelector('.menu');
 let menuButton = document.querySelector('.menu__button');
-let header= document.querySelector('.header__wrapper');
+let header = document.querySelector('.header__wrapper');
+let addressMap = document.querySelector('.address__map');
 
 headerMenu.classList.remove('menu--nojs');
 header.classList.remove('header__wrapper--nojs');
+addressMap.classList.remove('address__map--nojs');
 
 menuButton.addEventListener('click', function () {
   headerMenu.classList.toggle('menu--active')
